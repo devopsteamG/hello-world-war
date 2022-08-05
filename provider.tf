c20a4a4e892bdac524698be255fd9193c1d40ccf
Bharath <56950806+bablubharath@users.noreply.github.com>
2022-08-05 18:44:02 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,3 +1,16 @@
 provider "aws" {
   region = "eu-west-1"
 }
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">=3.37.0"
+    }
+    external = {
+      source  = "hashicorp/external"
+      version = "2.1.0"
+    }
+  }
+}
